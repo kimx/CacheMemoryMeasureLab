aa0db8c2e6bcf11d6056c3e3996e897caee1cf33
kimbud4net <kim@bud4.net>
2017-02-20 10:16:43 +0800

    iis admin lab

--- CacheMemoryMeasureLab.Web/Controllers/HomeController.cs
+++ CacheMemoryMeasureLab.Web/Controllers/HomeController.cs
@@ -1,23 +1,42 @@
-﻿using System;
+﻿using Microsoft.Web.Administration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Hosting;
 using System.Web.Mvc;
 
 namespace CacheMemoryMeasureLab.Web.Controllers
 {
     public class HomeController : Controller
     {
+        public static int IndexCount = 0;
         public ActionResult Index()
         {
+            IndexCount += 1;
+            ViewBag.IndexCount = IndexCount;
             return View();
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            //http://stackoverflow.com/questions/9470132/how-can-i-programmatically-recycle-a-net-web-apps-own-apppool
+            //Pooling要使用較高的權限,例:Local System
+            //將Pooling加入相關資料夾 C:\Windows\System32\inetsrv\config-->沒用  http://stackoverflow.com/questions/9470132/how-can-i-programmatically-recycle-a-net-web-apps-own-apppool
+            using (ServerManager iisManager = new ServerManager())
+            {
+                SiteCollection sites = iisManager.Sites;
+                foreach (Site site in sites)
+                {
+                    if (site.Name == HostingEnvironment.ApplicationHost.GetSiteName())
+                    {
+                        iisManager.ApplicationPools[site.Applications["/"].ApplicationPoolName].Recycle();
+                        break;
+                    }
+                }
+            }
 
-            return View();
+            return Redirect("Index");
         }
 
         public ActionResult Contact()
